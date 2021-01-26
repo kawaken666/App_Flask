3be75ca73d79649b86ad77a4ba6fd15b69a6a1aa
kawaken666 <kawaken2.666@gmail.com>
2021-01-27 01:37:31 +0900

    一覧表示の日付フィルタを実装

--- show_data/__init__.py
+++ show_data/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config.from_object('show_data.config')
@@ -8,13 +9,12 @@ app.config.from_object('show_data.config')
 # LoginManagerインスタンスを作成して初期化
 login_manager = LoginManager()
 login_manager.init_app(app)
-# TODO:要調査：鍵を作成？この鍵をcookieで交換している？
-app.config['SECRET_KEY'] = "secret"
+secret_key = generate_password_hash('secret key')
+app.config['SECRET_KEY'] = secret_key
 
 # SQLAlchemyインスタンスを作成して初期化
 db = SQLAlchemy(app)
 db.init_app(app)
 
-# TODO：要調査：Flaskをインスタンス化した後にしか、show_dataは参照(import)できない？
 # 以下を本ファイルの上部に移動するとエラー吐く
 import show_data.views.views
