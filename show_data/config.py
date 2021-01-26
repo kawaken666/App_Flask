36df4e9ea466722ac2e5d62e9cee7f34bc20b4ea
kawaken666 <kawaken2.666@gmail.com>
2021-01-27 03:55:32 +0900

    DBをsqliteからpostgresに変更するにあたり色々修正

--- show_data/config.py
+++ show_data/config.py
@@ -1,4 +1,9 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///show_data.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
+    'user': 'kk',
+    'password': 'kk',
+    'host': '127.0.0.1',
+    'name': 'show_data'
+})
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 SECRET_KEY = 'secret'
--- show_data/models/users.py
+++ show_data/models/users.py
@@ -5,8 +5,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True)
-    password_hash = db.Column(db.String(50))
+    email = db.Column(db.String(128), unique=True)
+    password_hash = db.Column(db.String(512))
 
     # セッションからidを取得してDBからユーザー情報を取得するコールバックデコレータ
     # flask-loginを使う際は必ず必要
