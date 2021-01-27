6dd4b93fc6e45af86c098d8e23730a6596b16eb4
kawaken666 <kawaken2.666@gmail.com>
2021-01-27 12:58:40 +0900

    configのDB接続先URIをheroku用に変更

--- show_data/config.py
+++ show_data/config.py
@@ -1,9 +1,20 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
+
+"""
+# ローカル用のDB接続URI
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
     'user': 'kk',
     'password': 'kk',
     'host': '127.0.0.1',
     'name': 'show_data'
 })
+"""
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 SECRET_KEY = 'secret'
